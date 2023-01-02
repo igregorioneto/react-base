bfc71e5649f02c822895da7e671b4397fc264073
Gregório Neto <igregoriodoneto@gmail.com>
2023-01-02 14:26:00 -0300

    Componentização

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
 function App() {
   const name = 'Teste';
   const age = 78;
 
   return (
     <>
-      <header>
-        <div className="bg-green-300 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">React Base</h1>
-        </div>
-      </header>
+      <Header>React Base</Header>
 
-      <main>
+      <Main>
         <div className="container mx-auto p-4">
           <p>
             o seu nome é {name}, com {name.length} caracteres e possui {age} anos.
           </p>
         </div>
-      </main>
+      </Main>
     </>
   );
 }
