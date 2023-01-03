177e6529b8632b0eb0a1a2054ce5d4fee200b6a9
Gregório Neto <igregoriodoneto@gmail.com>
2023-01-02 23:47:33 -0300

    Trabalhando com inputs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import TextInput from "./components/TextInput";
+import DateInput from "./components/DateInput";
 
 function App() {
-  const name = 'Teste';
-  const age = 78;
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(78);
+  const [birthDate, setBirthDate] = useState("2023-01-02");
+
+  function handleChangeName(newName) {
+    setName(newName);
+  }
+
+  function handleChangeBirthDate(newBirthDate) {
+    setBirthDate(newBirthDate);
+  }
 
   return (
     <>
       <Header>React Base</Header>
 
       <Main>
-        <div className="container mx-auto p-4">
+          <TextInput 
+            labelDescription="Digite o seu nome"
+            inputValue={name}
+            onInputChange={handleChangeName}
+          />
+          <DateInput
+            labelDescription="Digite sua data de nascimento"
+            inputValue={birthDate}
+            onInputChange={handleChangeBirthDate}
+          />
           <p>
             o seu nome é {name}, com {name.length} caracteres e possui {age} anos.
           </p>
-        </div>
       </Main>
     </>
   );
