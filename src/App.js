944a92095cf70247a03c5b1760fbba82c6d48156
Gregório Neto <igregoriodoneto@gmail.com>
2023-01-03 09:41:24 -0300

    Conversor data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import TextInput from "./components/TextInput";
 import DateInput from "./components/DateInput";
+import { getAgeFrom } from "./helpers/dateHelpers";
 
 function App() {
   const [name, setName] = useState('');
   const [age, setAge] = useState(78);
-  const [birthDate, setBirthDate] = useState("2023-01-02");
+  const [birthDate, setBirthDate] = useState("2022-01-02");
 
   function handleChangeName(newName) {
     setName(newName);
@@ -33,7 +34,7 @@ function App() {
             onInputChange={handleChangeBirthDate}
           />
           <p>
-            o seu nome é {name}, com {name.length} caracteres e possui {age} anos.
+            o seu nome é {name}, com {name.length} caracteres e possui {getAgeFrom(birthDate)} anos.
           </p>
       </Main>
     </>
